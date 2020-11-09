ae29e9f7a281fc17e3aa451e07b70be2893bb599
Helen Graham <helen.graham@food.gov.uk>
2020-11-09 17:02:36 +0000

    edit update file to include politeness check before scraping

--- update.R
+++ update.R
@@ -3,6 +3,8 @@ library(readr)
 library(purrr)
 library(dplyr)
 library(eulegscrape)
+library(polite)
+devtools::load_all()
 
 
 # read in table of URLs
@@ -14,11 +16,15 @@ urltable <- read_csv("reference/legislation-urls.csv")
 url2 <- urltable %>%
   mutate(latest = pmap_chr(., ~geturl(..2)))
 
-# which products need updating?
+# which products need updating (and is it OK to do so - polite check)?
 
 products <- url2 %>%
   filter(eurlex != latest) %>%
+  rowwise() %>%
+  mutate(pol = is.polite(bow(eurlex))) %>%
+  filter(pol == TRUE) %>%
   pull(product)
+
 
 if(length(products) > 0) {
   # source makefiles for these products
